bf8b1a4ac66d51f90404aaee09d8fb925405d309
Riba00 <jordiriba@iesebre.com>
2023-03-16 17:16:53 +0100

    WIP

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import axios from 'axios'
 
-//TOKEN soOqxnvCWQitKJc35I0VWHbG7sdOJoksWi1vgEzP
-
 const apiClient = axios.create({
     baseURL: 'http://casteachingriba.test/api',
     withCredentials: false,
     headers:{
         Accept: 'application/json',
         "Content-Type": 'application/json',
-        Authorization: 'Bearer wqzqef1N6lP587WfqRxPrwCNY9TSGaGAPblJpZsb'
+        Authorization: 'Bearer DC45c4yadItZ0jbSiVyDgKjHwsY1khp4ERREfput'
     }
 
 })
